e0c83fdf4944df0afaf37d05d1cc0f54a1b67599
PICTORY-droid <oceanlumes@gmail.com>
2026-04-28 16:24:32 +0900

    뒤로가기 BFCache 수정 - window.location.reload

--- app/page.tsx
+++ app/page.tsx
@@ -333,7 +333,7 @@ function HomeInner() {
   }, [pathname])
   useEffect(() => {
     const handlePageShow = (e: PageTransitionEvent) => {
-      if (e.persisted) { setLoading(true); fetchPrompts() }
+      if (e.persisted) { window.location.reload() }
     }
     window.addEventListener('pageshow', handlePageShow)
     return () => window.removeEventListener('pageshow', handlePageShow)
